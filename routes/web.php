b2b64ebdbcf15777cd52d910bff13345ca9f5d61
Zarul Zubir <zarul.izham@gmail.com>
2026-01-14 19:58:57 +0800

    Added default path

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use ZarulIzham\AutoDebit\Http\Controllers\CallbackController;
 
-$routePath = config('duitnow.callback.path');
-$routeName = config('duitnow.callback.name');
+$routePath = config('duitnow.callback.path', 'dnad/callback');
+$routeName = config('duitnow.callback.name', 'dnad.callback');
 
 Route::match(['get', 'post'], $routePath, CallbackController::class)->name($routeName);
